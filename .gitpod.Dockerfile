8427181a43e38428101aadce431df5be30e7d10f
Ofer Shaal <shaalofer@gmail.com>
2023-03-21 05:39:30 +0000

    fix: gitpod commands

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,4 +1,4 @@
 FROM gitpod/workspace-node-lts
 
 RUN sudo apt-get -qq update
-RUN sudo apt-get -qq install -y ffmpeg portaudio19-dev
+RUN sudo apt-get -qq install -y ffmpeg portaudio19-dev python3.8-venv
--- install_playground.sh
+++ install_playground.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+
 # install frontend
 cd interface
 yarn install
 
 # install and run backend
-cd ../backend && python3 -m venv venv
+cd ../backend
+python3 -m venv venv
 source ./venv/bin/activate
 pip install wheel
 pip install -r requirements.txt
